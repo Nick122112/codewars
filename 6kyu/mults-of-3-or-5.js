e40e245b4cd8fdbaa0564d91f3899e9e868321eb
Nicholas Kallas <kallasnicholas@gmail.com>
2022-07-25 17:04:02 -0500

    Added 6kyu solution

---
+++ 6kyu/mults-of-3-or-5.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+
+function solution(n) {
+  let nums = [];
+  for (let i = 1; i < n; i++) {
+    nums.push(i);
+  }
+  return nums
+    .filter((num) => num % 3 === 0 || num % 5 === 0)
+    .reduce((acc, cur) => acc + cur, 0);
+}
